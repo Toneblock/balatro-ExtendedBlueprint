7d93c26c10c0bc30cbc0a40523a4d5edad9d8195
senfinbrare <55140928+Toneblock@users.noreply.github.com>
2025-06-23 18:03:02 +0400

    fixes pt. 1

--- main.lua
+++ main.lua
@@ -7,6 +7,7 @@
 --- BADGE_COLOUR: 4b68ce
 --- DEPENDENCIES: [Steamodded>=1.0.0~ALPHA-1216c]
 --- VERSION: 0.3.3
+--- PRIORITY: -100
 
 ----------------------------------------------
 ------------MOD CODE -------------------------
@@ -19,104 +20,13 @@ function exb_parse()
 	end
 end
 
-
--- me when i copy the entire functions just to edit a couple lines
-function exb_get_flush(hand)
-	local ret = {}
-	local four_fingers = math.min(4, exb_amt("Four Fingers") or 0)
-	local suits = SMODS.Suit.obj_buffer
-	if #hand < (5 - (four_fingers)) then return ret else
-		for j = 1, #suits do
-			local t = {}
-			local suit = suits[j]
-			local flush_count = 0
-			for i=1, #hand do
-				if hand[i]:is_suit(suit, nil, true) then flush_count = flush_count + 1;  t[#t+1] = hand[i] end 
-			end
-			if flush_count >= (5 - (four_fingers)) then
-				table.insert(ret, t)
-				return ret
-			end
-		end
-		return {}
-	end
+-- we are overriding smods functions so priority should be low
+function SMODS.four_fingers()
+	return math.max(5 - exb_amt("Four Fingers"), 0)
 end
 
-function exb_get_straight(hand)
-	local ret = {}
-	local four_fingers = math.min(4, exb_amt("Four Fingers") or 0)
-	local can_skip = math.min(4, exb_amt("Shortcut") or 0)
-	if #hand < (5 - (four_fingers)) then return ret end
-	local t = {}
-	local RANKS = {}
-	for i = 1, #hand do
-		if hand[i]:get_id() > 0 then
-			local rank = hand[i].base.value
-			RANKS[rank] = RANKS[rank] or {}
-			RANKS[rank][#RANKS[rank] + 1] = hand[i]
-		end
-	end
-	local straight_length = 0
-	local straight = false
-	local skipped_rank = can_skip
-	local vals = {}
-	for k, v in pairs(SMODS.Ranks) do
-		if v.straight_edge then
-			table.insert(vals, k)
-		end
-	end
-	local init_vals = {}
-	for _, v in ipairs(vals) do
-		init_vals[v] = true
-	end
-	if not next(vals) then table.insert(vals, 'Ace') end
-	local initial = true
-	local br = false
-	local end_iter = false
-	local i = 0
-	while 1 do
-		end_iter = false
-		if straight_length >= (5 - (four_fingers)) then
-			straight = true
-		end
-		i = i + 1
-		if br or (i > #SMODS.Rank.obj_buffer + 1) then break end
-		if not next(vals) then break end
-		for _, val in ipairs(vals) do
-			if init_vals[val] and not initial then br = true end
-			if RANKS[val] then
-				straight_length = straight_length + 1
-				skipped_rank = can_skip
-				for _, vv in ipairs(RANKS[val]) do
-					t[#t + 1] = vv
-				end
-				vals = SMODS.Ranks[val].next
-				initial = false
-				end_iter = true
-				break
-			end
-		end
-		if not end_iter then
-			local new_vals = {}
-			for _, val in ipairs(vals) do
-				for _, r in ipairs(SMODS.Ranks[val].next) do
-					table.insert(new_vals, r)
-				end
-			end
-			vals = new_vals
-			if can_skip and skipped_rank > 0 then
-				skipped_rank = skipped_rank - 1
-			else
-				straight_length = 0
-				skipped_rank = can_skip
-				if not straight then t = {} end
-				if straight then break end
-			end
-		end
-	end
-	if not straight then return ret end
-	table.insert(ret, t)
-	return ret
+function SMODS.shortcut()
+	return exb_amt("Shortcut")
 end
 
 -- we hookin
@@ -141,8 +51,10 @@ end
 
 -- i like coding because you can put your spaghetti in a function and suddenly it's clean code
 function exb_amt(_name, amt)
-	if amt and (G.GAME.exb and G.GAME.exb[_name]) and G.GAME.exb[_name] >= amt then return true end
-	if not amt and (G.GAME.exb and G.GAME.exb[_name]) then return G.GAME.exb[_name] end
+	if not G.GAME.exb then G.GAME.exb = {} end
+	if not G.GAME.exb[_name] then G.GAME.exb[_name] = 0 end
+	if amt and G.GAME.exb[_name] >= amt then return true end
+	if not amt then return G.GAME.exb[_name] end
 end
 
 function exb_truefacecheck(_card)
